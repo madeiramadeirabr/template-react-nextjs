d50436c6d9e27dd3a577a8f1052a6e4570b8ce64
Jilles Cardoso <jillesmc@gmail.com>
2021-09-17 16:36:25 -0300

    fix: change api return object to match the type

--- src/pages/api/about.ts
+++ src/pages/api/about.ts
@@ -1,10 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import type User from '@type/user'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { User } from '@type/user';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<User>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(_req: NextApiRequest, res: NextApiResponse<User>) {
+  res.status(200).json({
+    createdAt: '2021-09-09',
+    id: '123',
+    active: true,
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'email@domain.com',
+    phone: '5541999999999',
+  });
 }
