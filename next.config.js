06d9c738401afcc5260e79801069f2c187480983
Jilles Cardoso <jillesmc@gmail.com>
2021-09-17 11:37:15 -0300

    feat(nextjs): add custom config and include plugins

--- next.config.js
+++ next.config.js
@@ -1,4 +1,52 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withPlugins = require('next-compose-plugins');
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  optimizeFonts: true,
   reactStrictMode: true,
-}
+  poweredByHeader: false,
+  async headers() {
+    return [];
+  },
+  async rewrites() {
+
+    const rewrites = {
+      beforeFiles: [
+        // These rewrites are checked after headers/redirects
+        // and before all files including _next/public files which
+        // allows overriding page files
+      ],
+      afterFiles: [
+        // These rewrites are checked after pages/public files
+        // are checked but before dynamic routes
+      ],
+      // After checking all Next.js pages (including dynamic routes)
+      // and static files we proxy any other requests
+      fallback: [
+      ],
+    };
+    if (NEXT_PUBLIC_APP_STAGE === envs.DEVELOPMENT) {
+      // eslint-disable-next-line no-console
+      console.info('Using  rewrites:', rewrites);
+    }
+    return rewrites;
+  },
+};
+
+module.exports = withPlugins(
+  [
+    [withBundleAnalyzer],
+    [
+      reactSvg,
+      {
+        include: path.resolve(__dirname, './src/static'),
+      },
+    ],
+  ],
+  nextConfig
+);
